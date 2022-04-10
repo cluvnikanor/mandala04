82a9d93cc7fe6f6e7bffc106fa529328c4b085d2
Scion Landes <55755526+cluvnikanor@users.noreply.github.com>
2022-04-10 10:18:23 +0300

    02

--- src/main/java/com/sl/mdb04/Mdb04Application.java
+++ src/main/java/com/sl/mdb04/Mdb04Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Mdb04Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mdb04Application.class, args);
+		SpringApplication.run(Mdb04Application.class, args);  
 	}
 
 }
